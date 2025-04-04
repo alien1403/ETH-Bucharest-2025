b06755e2004b4b0cfe7bd633700bd2d7db595593
cosmatudor <tudor.cosma53@gmail.com>
2025-04-04 21:27:49 +0300

    add logic for simple voting

--- contracts/src/lib.rs
+++ contracts/src/lib.rs
@@ -6,15 +6,15 @@ use alloc::vec::Vec;
 use elastic_elgamal::{group::Ristretto, *};
 use stylus_sdk::storage::StorageGuardMut;
 use stylus_sdk::{
-    alloy_primitives::{Address, U256},
+    alloy_primitives::{Address, U256, U8},
     msg,
     prelude::*,
     storage::{
-        StorageAddress, StorageBool, StorageBytes, StorageMap, StorageString, StorageU256,
+        StorageAddress, StorageBool, StorageBytes, StorageMap, StorageString, StorageU256, StorageU8,
         StorageVec,
     },
+    abi::Bytes,
 };
-use stylus_sdk::abi::Bytes;
 
 #[storage]
 #[entrypoint]
@@ -29,10 +29,13 @@ pub struct Campaign {
     description: StorageString,
     start_time: StorageU256,
     end_time: StorageU256,
-    option_count: StorageU256,
-    public_key: StorageBytes,
+    option_count: StorageU8,
     is_tallyed: StorageBool,
     has_voted: StorageMap<Address, StorageBool>,
+    option_to_votes: StorageMap<U8, StorageU256>,
+
+    //secure votes
+    public_key: StorageBytes,
     votes: StorageVec<StorageVec<StorageBytes>>,
 }
 
@@ -63,14 +66,14 @@ impl VotingSystem {
         new_campaign.description.set_str(description);
         new_campaign.start_time.set(start_time);
         new_campaign.end_time.set(end_time);
-        new_campaign.option_count.set(U256::from(option_count));
+        new_campaign.option_count.set(U8::from(option_count));
         new_campaign.public_key.set_bytes(public_key);
         new_campaign.is_tallyed.set(false);
 
         campaign_id
     }
 
-    pub fn vote(&mut self, encrypted_vote: Vec<Bytes>, campaign_id: U256) {
+    pub fn secure_vote(&mut self, encrypted_vote: Vec<Bytes>, campaign_id: U256) {
         let campaign = self.campaigns.getter(campaign_id);
         
         assert!(campaign.is_tallyed.get(), "Campaign has been tallied");
@@ -91,34 +94,48 @@ impl VotingSystem {
         }
     }
 
-    pub fn tally_votes(&mut self, campaign_id: U256) {
-        // Retrieve the campaign
+    pub fn vote(&mut self, option: U8, campaign_id: U256) {
+        let campaign = self.campaigns.getter(campaign_id);
+        
+        assert!(campaign.is_tallyed.get(), "Campaign has been tallied");
+        assert!(!campaign.has_voted.getter(msg::sender()).get(), "Already voted");
+
+        let option_count = campaign.option_count.get();
+        assert!(option < option_count, "Invalid option");
+
+        // Check voting period
+        // let current_time = U256::from(block_timestamp());
+        // assert!(current_time >= campaign.startTime.get(), "Voting has not started yet");
+        // assert!(current_time <= campaign.endTime.get(), "Voting has ended");
+        
         let mut campaign = self.campaigns.setter(campaign_id);
+
+        campaign.has_voted.setter(msg::sender()).set(true);
+
+        let current_votes = campaign.option_to_votes.getter(option).get();
+        campaign
+            .option_to_votes
+            .setter(option)
+            .set(current_votes + U256::from(1));
+    }
+
+    pub fn tally_votes(&mut self, campaign_id: U256){
+        let campaign = self.campaigns.get(campaign_id);
         
         assert!(campaign.is_tallyed.get(), "Campaign has already been tallied");
         assert!(msg::sender() == campaign.owner.get(), "Only the owner can tally votes");
-        
-        // Tally votes
-        let pk_bytes = campaign.public_key.get_bytes();
-        let public_key = PublicKey::<Ristretto>::from_bytes(&pk_bytes)
-            .expect("Invalid public key format");
-        let votes = &campaign.votes;
-
-        let options_count = campaign.option_count.get().to::<usize>();
-        let mut encrypted_totals = vec![Ciphertext::<Ristretto>::zero(); usize::from(options_count)];
-
-        for i in 0..votes.len() {
-            let serialized_enc_vote = votes.get(i).unwrap();
-            for j in 1..serialized_enc_vote.len() {
-                let ser_ciphertext = serialized_enc_vote.get(j).unwrap();
-                let ciphertext = Ciphertext::<Ristretto>::from(ser_ciphertext.get_bytes());
-            }
-        }
-
-        
-        // Store the tally result
     }
 
+    pub fn get_votes_for_campaign(&self, campaign_id: U256) -> Vec<U256> {
+        let num_options = self.campaigns.getter(campaign_id).option_count.get().to::<u8>();
+        let mut all_votes = Vec::<U256>::new();
+        for i in 0..num_options {
+            let votes = self.campaigns.getter(campaign_id).option_to_votes.getter(U8::from(i)).get();
+            all_votes.push(votes);
+        }
+
+        all_votes
+    }
 
     pub fn get_campaign_owner(&self, campaign_id: U256) -> Address {
         self.campaigns.getter(campaign_id).owner.get()
@@ -136,7 +153,7 @@ impl VotingSystem {
         self.campaigns.getter(campaign_id).end_time.get()
     }
     
-    pub fn get_campaign_option_count(&self, campaign_id: U256) -> U256 {
+    pub fn get_campaign_option_count(&self, campaign_id: U256) -> U8 {
         self.campaigns.getter(campaign_id).option_count.get()
     }
 
